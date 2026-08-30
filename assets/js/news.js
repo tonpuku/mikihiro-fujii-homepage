0f5c01018fc63f7bb0b5eb225daaa8507c88814a
tonpuku <tonpuku.kyushu.u@gmail.com>
2026-08-30 22:57:11 +0900

    Correct Kobe talk date to September 4

--- assets/js/news.js
+++ assets/js/news.js
@@ -16,15 +16,15 @@ window.NEWS = [
   {
     category: "talks",
     date: "2026-08-16",
-    eventDate: "2026-09-03",
+    eventDate: "2026-09-04",
     href: "https://www.mathsoc.jp/activity/meeting/kobe26sept/index.html",
     linkText: {
       en: "2026 MSJ Autumn Meeting",
       ja: "2026年度秋季総合分科会"
     },
     text: {
-      en: "On September 3, 2026, I will give a talk at the 2026 MSJ Autumn Meeting (Kobe University).",
-      ja: "2026年9月3日に2026年度秋季総合分科会(神戸大学)にて講演します．"
+      en: "On September 4, 2026, I will give a talk at the 2026 MSJ Autumn Meeting (Kobe University).",
+      ja: "2026年9月4日に2026年度秋季総合分科会(神戸大学)にて講演します．"
     }
   },
   {
