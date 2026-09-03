7a7f8e422604b52af9d29a9cdacf4c1a2cc7041c
tonpuku <tonpuku.kyushu.u@gmail.com>
2026-09-04 07:34:06 +0900

    Remove completed MSJ autumn talk news

--- assets/js/news.js
+++ assets/js/news.js
@@ -31,20 +31,6 @@ window.NEWS = [
   },
   {
     category: "talks",
-    date: "2026-08-16",
-    eventDate: "2026-09-04",
-    href: "https://www.mathsoc.jp/activity/meeting/kobe26sept/index.html",
-    linkText: {
-      en: "2026 MSJ Autumn Meeting",
-      ja: "2026年度秋季総合分科会"
-    },
-    text: {
-      en: "On September 4, 2026, I will give a talk at the 2026 MSJ Autumn Meeting (Kobe University).",
-      ja: "2026年9月4日に2026年度秋季総合分科会(神戸大学)にて講演します．"
-    }
-  },
-  {
-    category: "talks",
     date: "2026-08-23",
     eventDate: "2026-10-09",
     href: "https://www2.math.kyushu-u.ac.jp/FE-Seminar/",
